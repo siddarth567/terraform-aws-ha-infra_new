b2ef6eaaa232f3857fb94f1d8af9f6485e1e20b8
siddarth567 <ksiddharth263@gmail.com>
2026-07-16 10:17:37 +0530

    Remove unsupported use_lockfile from backend config

--- backend.tf
+++ backend.tf
@@ -15,7 +15,6 @@ terraform {
     key            = "infrastructure/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    use_lockfile   = true      # replaces deprecated dynamodb_table (requires S3 versioning)
 
     # Enable S3 bucket versioning for state file history
     # These settings should be configured on the S3 bucket itself:
